9eaae219ec14584cd8125276c5d82ca9564ca0c0
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-05-25 02:37:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// npx kill-port 5000
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.remhw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('AgroBhai').collection('tool');
+
+        app.get('/tool', async (req, res) =>{
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+        app.get('/tool/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        })
+    }
+    finally {
+        
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("It's working nicely!")
+});
+
+app.get('/users', (req, res) => {
+    res.send("It's from users!")
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
