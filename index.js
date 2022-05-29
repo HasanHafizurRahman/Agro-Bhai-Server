2e3b7b658ac47f81505b825d2a2997fcbf9583fa
HasanHafizurRahman <hasanshanto922@gmail.com>
2022-05-29 21:20:43 +0600

    procfile

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.remhw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden  Access'})
+        }
+        console.log(decoded) // bar;
+    })
+}
 async function run() {
     try {
         await client.connect();
@@ -28,7 +42,27 @@ async function run() {
             const tools = await cursor.toArray();
             res.send(tools);
         })
+        app.get('/user', async(req, res) =>{
+            const users = await userCollection.find().toArray();
+            res.send(users)
+        })
+        // app.get('/admin/:email', async (req, res) =>{
+        //     const email = req.params.email;
+        //     const user = await userCollection.findOne({email: email});
+        //     const isAdmin = user.role === 'admin';
+        //     res.send({admin: isAdmin});
+        // })
 
+        app.put('/user/admin/:email', async(req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: {role:'admin'},
+            };
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result);
+            
+        })
         app.put('/user/:email', async(req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -49,8 +83,10 @@ async function run() {
         })
 
         app.get('/myOrder', async (req, res) =>{
-            const email = req.query.email;
-            const query = {email: email}
+            const user = req.query.user;
+            const authorization = req.headers.authorization;
+            console.log('auth headers', authorization);
+            const query = {user: user}
             const cursor = myOrderCollection.find(query);
             const myOrder = await cursor.toArray();
             res.send(myOrder);
